ab916ec302450fdbef00cea70678f1a26a5f120e
りゅう <ryu-ookouchi-20h@stu.kbc.ac.jp>
2022-07-26 18:55:11 +0900

    Game settings
    ゲーム内容変更
    クリア画面追加
    ゲームオーバー画面追加
    シーン遷移追加

--- Chicken_Lace/SceneManager.h
+++ Chicken_Lace/SceneManager.h
@@ -4,8 +4,11 @@
 typedef enum {
 	SCENE_TITLE,	//タイトル画面
 	SCENE_GAME,		//ゲーム画面
+	SCENE_CLEAR,
+	SCENE_GAMEOVER,
 	SCENE_HELP,		//ヘルプ画面
 	SCENE_RANKING,	//ランキング画面（表示）
+	SCENE_RANKINPUT,//ランキング入力
 	SCENE_END,		//エンド画面
 	SCENE_STOP,		//ストップ
 	SCENE_NONE,		//シーン無し
@@ -16,6 +19,7 @@ void SceneManager_Initialize();	//初期化
 void SceneManager_Finalize();	//終了
 void SceneManager_Update();		//更新
 void SceneManager_Draw();		//描画
+
 //引数nextSceneにシーンを変更する
 void SceneManager_ChangeScene(SCENE nextScene);
 

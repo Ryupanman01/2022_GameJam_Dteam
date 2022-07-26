3046efd1909b6d7c651a699f5a983b1fa48190d0
逃げ足のサンジ <106945313+Ajitomi@users.noreply.github.com>
2022-07-26 12:03:23 +0900

    タイトルの変更
    カーソルを三角から文字を囲む四角に変更。
    タイトルネーム用の画像[Title2.png]を読込み、追加。
    セレクト中は文字が拡大されて表示されるようにした。
    決定ボタンをAに変更した。

--- Chicken_Lace/Title.cpp
+++ Chicken_Lace/Title.cpp
@@ -4,16 +4,19 @@
 #include "Input.h"
 
 static int TitleImage;		//タイトル画像格納
+static int TitleImage2;		//タイトル画像2格納
 int TitleBGM, SelectSE, ClickSE;
 static int MenuNumber = 0;
 int PosY;
-static int count = 0;
+static int count = 0;		
 int ButtonFlag;
 
 //初期化
 void Title_Initialize() {
 	//画像読み込み
 	TitleImage = LoadGraph("Images/Title1.png");
+	//画像読込み
+	TitleImage2 = LoadGraph("Images/Title2.png");
 	//タイトルBGM読込み
 	TitleBGM = LoadSoundMem("Sound/TitleBGM.mp3");
 	//セレクトSE読込み
@@ -31,7 +34,7 @@ void Title_Finalize() {
 
 //更新
 void Title_Update() {
-	if (iKeyFlg & PAD_INPUT_B) {
+	if (iKeyFlg & PAD_INPUT_A) {
 		switch (MenuNumber)
 		{
 		case 0:
@@ -60,6 +63,7 @@ void Title_Update() {
 void Title_Draw() {
 	//画像配置
 	DrawGraph(0, 0, TitleImage, FALSE);
+	DrawGraph(0, 15, TitleImage2, TRUE);
 	PlaySoundMem(TitleBGM, DX_PLAYTYPE_LOOP, FALSE);
 
 	//メニューカーソル移動処理
@@ -74,33 +78,49 @@ void Title_Draw() {
 		if (--MenuNumber < 0)MenuNumber = 3;
 	}
 
-	////デバッグ用Line
-	//DrawLine(0, 240, 640, 240, 0x000000, 1);
-	//DrawLine(320, 0, 320, 480, 0x000000, 1);
+	//デバッグ用Line
+	DrawLine(0, 240, 640, 240, 0x000000, 1);
+	DrawLine(320, 0, 320, 480, 0x000000, 1);
 
 	SetFontSize(55);
-	DrawString(114, 100, "チキンレース！！", 0x000000);
-	SetFontSize(35);
-	DrawString(190, 250, "ゲームスタート！", 0x000000);
-	DrawString(228, 300, "ランキング", 0x000000);
-	DrawString(268, 350, "ヘルプ", 0x000000);
-	DrawString(268, 400, "エンド", 0x000000);
+	////デバッグ用タイトル
+	//DrawString(114, 100, "チキンレース！！", 0x000000);
+	SetFontSize(34);
+	if(MenuNumber!=0)DrawString(192, 254, "ゲームスタート！", 0x000000);
+	if(MenuNumber!=1)DrawString(230, 304, "ランキング", 0x000000);
+	if(MenuNumber!=2)DrawString(268, 354, "ヘルプ", 0x000000);
+	if(MenuNumber!=3)DrawString(268, 400, "エンド", 0x000000);
 
-	PosY = MenuNumber * 50;
+	SetFontSize(45);
+	PosY = MenuNumber * 49;
 	if (MenuNumber == 0)
 	{
-		DrawTriangle(150, 245 + PosY, 180, 265 + PosY, 150, 285 + PosY, GetColor(255, 0, 0), TRUE);
-		DrawTriangle(153, 250 + PosY, 175, 265 + PosY, 153, 280 + PosY, 0x000000, TRUE);
+		DrawBox(126, 237, 502, 288, 0x989898, TRUE);
+		DrawString(132, 240, "ゲームスタート！", 0xffffff);
+		//DrawTriangle(112, 245 + PosY, 142, 265 + PosY, 112, 285 + PosY, GetColor(255, 0, 0), TRUE);
+		//DrawTriangle(115, 250 + PosY, 137, 265 + PosY, 115, 280 + PosY, 0x000000, TRUE);
 	}
 	else if (MenuNumber == 1)
 	{
-		DrawTriangle(190, 250 + PosY, 220, 270 + PosY, 190, 290 + PosY, GetColor(255, 0, 0), TRUE);
-		DrawTriangle(193, 255 + PosY, 215, 270 + PosY, 193, 285 + PosY, 0x000000, TRUE);
+		DrawBox(193, 292, 440, 343, 0x989898, TRUE);
+		DrawString(199, 295, "ランキング", 0xffffff);
+		//DrawTriangle(161, 250 + PosY, 191, 270 + PosY, 161, 290 + PosY, GetColor(255, 0, 0), TRUE);
+		//DrawTriangle(164, 255 + PosY, 186, 270 + PosY, 164, 285 + PosY, 0x000000, TRUE);
 	}
-	else if (MenuNumber == 2 || MenuNumber == 3)
+	else if (MenuNumber == 2)
 	{
-		DrawTriangle(230, 250 + PosY, 260, 270 + PosY, 230, 290 + PosY, GetColor(255, 0, 0), TRUE);
-		DrawTriangle(233, 255 + PosY, 255, 270 + PosY, 233, 285 + PosY, 0x000000, TRUE);
+		DrawBox(247, 342, 400, 393, 0x989898, TRUE);
+		DrawString(250, 345, "ヘルプ", 0xffffff);
+		//DrawTriangle(212, 250 + PosY, 242, 270 + PosY, 212, 290 + PosY, GetColor(255, 0, 0), TRUE);
+		//DrawTriangle(215, 255 + PosY, 237, 270 + PosY, 215, 285 + PosY, 0x000000, TRUE);
 	}
+	else if (MenuNumber == 3)
+	{
+		DrawBox(247, 392, 400, 443, 0x989898, TRUE);
+		DrawString(255, 395, "エンド", 0xffffff);
+		//DrawTriangle(212, 250 + PosY, 242, 270 + PosY, 212, 290 + PosY, GetColor(255, 0, 0), TRUE);
+		//DrawTriangle(215, 255 + PosY, 237, 270 + PosY, 215, 285 + PosY, 0x000000, TRUE);
+	}
+
 
 }
